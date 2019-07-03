83043ef89455976844f3288c9091a8c2a5fd8648
Oleg Pyatin <dllwork2014@yandex.ru>
2019-07-03 17:16:24 +0300

    firebase version fix

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-messaging.js');
 
 firebase.initialize({
     messagingSenderId: '17465665194'    
